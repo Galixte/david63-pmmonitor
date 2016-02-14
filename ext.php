0d7ce1ede98105cef39efbdafa79b412a1fae1df
david63 <david.wood@pear-tree.org.uk>
2016-02-14 21:43:33 +0000

    Updated ext file

--- ext.php
+++ ext.php
@@ -11,10 +11,12 @@ namespace david63\pmmonitor;
 
 class ext extends \phpbb\extension\base
 {
-	const PM_MONITOR_VERSION	= '2.1.0';
+	const PM_MONITOR_VERSION = '2.1.0';
 
 	/**
 	* Enable extension if phpBB version requirement is met
+	*
+	* @var string Require 3.2.0-a1 due to updated 3.2 syntax
 	*
 	* @return bool
 	* @access public
@@ -22,6 +24,6 @@ class ext extends \phpbb\extension\base
 	public function is_enableable()
 	{
 		$config = $this->container->get('config');
-		return version_compare($config['version'], '3.2.0-a1', '>=');
+		return phpbb_version_compare($config['version'], '3.2.0-a1', '>=');
 	}
 }
